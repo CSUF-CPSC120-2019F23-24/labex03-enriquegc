3afeb1bca39769d201a2d3ca28efd47182b64a79
enriquegc2001 <enriquegc2001@csu.fullerton.edu>
2019-09-07 14:07:00 -0700

    I made a height calculator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,23 @@
 // This program calculates a person's height in feet (') and inches (").
+
+#include <iostream>
+
+int main()
+{
+  int height;
+  double inches;
+  double official_height;
+
+  //input the price of your total height
+  std::cout << "please enter the person's height in inches:";
+  std::cin >> height;
+
+  // calculates the entered height
+  official_height = height / 12;
+
+// calculates the entered height if it does not divide perfectly by 12
+  inches = height % 12;
+
+  std::cout << "that person is:" << official_height << "\'" << inches << "\"";
+
+}
